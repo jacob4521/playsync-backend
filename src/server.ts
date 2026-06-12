464f60a17a032202361a333074c5e1abb511b3f5
Zakib <zakib6354@gmail.com>
2026-06-12 23:06:50 +0530

    Added tsx as the code watcher for the code changes in development

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 
 // Create an instance of the express application
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
