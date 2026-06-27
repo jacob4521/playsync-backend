a6ace2ffc40f74098b0d960b275bb775122b437f
Zakib <zakib6354@gmail.com>
2026-06-27 16:22:54 +0530

    feat: add internal routing for booking and arena management with authentication middleware

--- src/routes/internalRoutes.ts
+++ src/routes/internalRoutes.ts
@@ -1,13 +1,25 @@
 import express from "express";
-import { verifyInternalServer } from "../middlewares/authMiddleware.js";
 import {
+  verifyInternalServer,
+  authenticateToken,
+} from "../middlewares/authMiddleware.js";
+import {
+  createBooking,
   getAvailability,
   getBookingsByPlayerId,
 } from "../controllers/bookingController.js";
+import { getArenas } from "../controllers/arenaController.js";
 
 const router = express.Router();
 
 router.get("/bookings/:userId", verifyInternalServer, getBookingsByPlayerId);
 router.get("/availability", verifyInternalServer, getAvailability);
+router.get("/arenas", verifyInternalServer, getArenas);
+router.post(
+  "/add-booking",
+  verifyInternalServer,
+  authenticateToken,
+  createBooking,
+);
 
 export default router;
